a274e75427068e52c2f39ef0384318e18a819dd0
HSoar1 <soarshizu@gmail.com>
2021-12-18 06:08:29 +0900

    [add] AtCoderBeginnerContest230/AtCoderQuiz3.go

---
+++ AtCoderBeginnerContest230/AtCoderQuiz3.go
@@ -0,0 +1,17 @@
+package AtCoderBeginnerContest230
+
+import (
+	"fmt"
+)
+
+func main() {
+	var N int
+	fmt.Scan(&N)
+	if N < 10 {
+		fmt.Printf("AGC00%d", N)
+	} else if N < 42 {
+		fmt.Printf("AGC0%d", N)
+	} else {
+		fmt.Printf("AGC0%d", N+1)
+	}
+}
