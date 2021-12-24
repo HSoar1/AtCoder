f4a6ee09acd654cc3c77bc504f7c3c4810535eb4
HSoar1 <soarshizu@gmail.com>
2021-12-24 13:34:36 +0900

    [add] URL and abc230 C - X drawing

---
+++ AtCoderBeginnerContest230/CXDrawing/CXDrawing.go
@@ -0,0 +1,14 @@
+package main
+
+//https://atcoder.jp/contests/abc230/tasks/abc230_c
+
+import (
+	"fmt"
+)
+
+func main(){
+	var N, A, B, P, Q, R, S int64
+	fmt.Scanf("%d %d %d",&N, &A, &B)
+	fmt.Scanf("%d %d %d %d",&P, &Q, &R, &S)
+	var result string[][]
+}
